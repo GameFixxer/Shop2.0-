12bfe4a2faa0b4e003ce61a82b05b5b43ae0bdb3
= <=r.berndt@nexus-united.com>
2020-10-23 13:36:26 +0200

    some cool safety features

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -4,6 +4,7 @@
 namespace App\Controller;
 
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,19 +12,24 @@ use Symfony\Component\Routing\Annotation\Route;
 class CommentController extends AbstractController
 {
     /**
-     * @Route("/comments/{id}/vote/{direction}")
+     * @Route("/comments/{id}/vote/{direction<up|down>}", methods="POST")
      * @param int $id
      * @param string $direction
+     * @param LoggerInterface $logger
      * @return JsonResponse
      */
-    public function commentVote(int $id, string $direction)
+
+
+    public function commentVote(int $id, string $direction, LoggerInterface $logger)
     {
         //todo use id to query db
         //use real logic when db is ready
         if($direction === 'up')
         {
+            $logger->info('Voting up');
             $currentVoteCount = rand(7,100);
         }else{
+            $logger->info('Voting down');
             $currentVoteCount = rand(0,5);
         }
         return $this->json(['votes' => $currentVoteCount]);
